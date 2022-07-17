bcf752f3e5320edea3e61d09f9e0d7516ac017e1
CarlosJ <carlos.jimenez@pangea.es>
2022-07-17 15:11:21 +0200

    Chore: Fix AbstractHttpViewModelPort

--- src/Boilerwork/System/Http/AbstractHttpViewModelPort.php
+++ src/Boilerwork/System/Http/AbstractHttpViewModelPort.php
@@ -5,16 +5,10 @@ declare(strict_types=1);
 
 namespace Boilerwork\System\Http;
 
-use Boilerwork\Application\CommandBus;
 use Psr\Http\Message\ResponseInterface;
 use Boilerwork\System\Http\Request;
 
-abstract class AbstractHttpPort
+abstract class AbstractHttpViewModelPort
 {
     abstract public function __invoke(Request $request, array $vars): ResponseInterface;
-
-    final public function command(): CommandBus
-    {
-        return new CommandBus();
-    }
 }
