4dfb77266e6bb61191081637da8458e4515b0601
CarlosJ <carlos.jimenez@pangea.es>
2022-08-21 17:12:44 +0200

    refactor: Remove equals method from ValueObject Abstract Class

--- src/Boilerwork/Domain/ValueObjects/ValueObject.php
+++ src/Boilerwork/Domain/ValueObjects/ValueObject.php
@@ -8,6 +8,4 @@ namespace Boilerwork\Domain\ValueObjects;
 abstract class ValueObject
 {
     abstract public function toPrimitive(): mixed;
-
-    abstract public function equals(ValueObject $object): bool;
 }
