17e467cea2c9782564fd66c3043098cfd260e9f1
CarlosJ <carlos.jimenez@pangea.es>
2022-08-22 20:57:58 +0200

    Check health environment variables

--- src/Boilerwork/System/Health/CheckHealthPort.php
+++ src/Boilerwork/System/Health/CheckHealthPort.php
@@ -16,7 +16,15 @@ final class CheckHealthPort extends AbstractHttpPort
     {
         return Response::json([
             'appName' => $_ENV['APP_NAME'],
-            'status' => 'OK'
+            'status' => 'OK',
+            'appName' => isset($_ENV['APP_NAME']) ? $_ENV['APP_NAME'] : 'nop',
+            'MESSAGE_BROKER_HOST' => isset($_ENV['MESSAGE_BROKER_HOST']) ? $_ENV['MESSAGE_BROKER_HOST'] : 'nope',
+            'POSTGRESQL_READS_HOST' => isset($_ENV['POSTGRESQL_READS_HOST']) ? $_ENV['POSTGRESQL_READS_HOST'] : 'nope',
+            'POSTGRESQL_READS_USERNAME' => isset($_ENV['POSTGRESQL_READS_USERNAME']) ? $_ENV['POSTGRESQL_READS_USERNAME'] : 'nope',
+            'POSTGRESQL_READS_PASSWORD' => isset($_ENV['POSTGRESQL_READS_PASSWORD']) ? $_ENV['POSTGRESQL_READS_PASSWORD'] : 'nope',
+            'POSTGRESQL_WRITES_HOST' => isset($_ENV['POSTGRESQL_WRITES_HOST']) ? $_ENV['POSTGRESQL_WRITES_HOST'] : 'nope',
+            'POSTGRESQL_WRITES_USERNAME' => isset($_ENV['POSTGRESQL_WRITES_USERNAME']) ? $_ENV['POSTGRESQL_WRITES_USERNAME'] : 'nope',
+            'POSTGRESQL_WRITES_PASSWORD' => isset($_ENV['POSTGRESQL_WRITES_PASSWORD']) ? $_ENV['POSTGRESQL_WRITES_PASSWORD'] : 'nope',
         ]);
     }
 }
