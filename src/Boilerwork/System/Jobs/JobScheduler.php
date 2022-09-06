e97e7f342309cb61e1b1b3f8d568a31b7189357f
CarlosJ <carlos.jimenez@pangea.es>
2022-09-06 19:29:50 +0200

    chore: Add https://github.com/peppeocchi/php-cron-scheduler as JobScheduler and refactor methods

--- src/Boilerwork/System/Jobs/JobScheduler.php
+++ src/Boilerwork/System/Jobs/JobScheduler.php
@@ -6,21 +6,38 @@ declare(strict_types=1);
 namespace Boilerwork\System\Jobs;
 
 use App\Shared\Providers\JobsProvider;
+use Boilerwork\Infra\Persistence\Adapters\Redis\RedisClient;
 use Boilerwork\System\IsProcessInterface;
+use DateTime;
+use GO\Job;
+use GO\Scheduler;
 use Swoole\Process;
 
+
+/**
+ * We are using GO\Scheduler to check due times and executing jobs
+ * @uses https://github.com/peppeocchi/php-cron-scheduler
+ *
+ *
+ */
 final class JobScheduler implements IsProcessInterface
 {
     private Process $process;
 
-    private const LOOP_INTERVAL = 5; // Set Maximum each 30 seconds or task set to be repeated each minute may not be executed
+    private const LOOP_INTERVAL = 60; // Execute each 60 seconds as is the default cron job interval
 
-    public function __construct(private JobsProvider $jobsProvider)
+    public function __construct()
     {
+        $this->scheduler = new Scheduler();
+        $this->client = new RedisClient();
+
+        $this->addJobs();
+
         $this->process = (new Process(
             function () {
+
                 while (true) {
-                    $this->jobsProvider->run();
+                    $this->check();
                     // Use native sleep only with Swoole hooks enabled
                     sleep(self::LOOP_INTERVAL);
                 }
@@ -32,5 +49,55 @@ final class JobScheduler implements IsProcessInterface
     public function process(): Process
     {
         return $this->process;
+    }
+
+    /**
+     * Check is job is Due an must be executed
+     */
+    private function check()
+    {
+        foreach ($this->scheduler->getQueuedJobs() as $job) {
+            if ($job->isDue(new DateTime()) === true) {
+                $this->executeIfNotOverlapped($job);
+            }
+        }
+    }
+
+    /**
+     * Check in Redis if Job is already beeing processed to aboid overlapping with
+     * other posible instances of the same microservice.
+     */
+    private function executeIfNotOverlapped(Job $job): void
+    {
+        go(function () use ($job) {
+            $jobId = $job->getId();
+            $this->client->getConnection();
+
+            if ($this->client->get($jobId) === false) {
+
+                $this->client->set($jobId, 'JobInProgress');
+
+                $job->run();
+
+                var_dump($job->getOutput());
+
+                $this->client->del($jobId);
+            }
+
+            $this->client->putConnection();
+        });
+    }
+
+    private function addJobs(): void
+    {
+        foreach (JobsProvider::$jobs as $job) {
+            $interval = $job[1][0];
+            $args = $job[1][1];
+
+            $this->scheduler->call(
+                fn: [new $job[0], 'handle'],
+                id: $job[0],
+            )->$interval($args);
+        }
     }
 }
