41c45ab25f1e3f3ddd427b84fc181109be180fd1
CarlosJ <carlos.jimenez@pangea.es>
2023-06-05 19:22:01 +0200

    refactor(Crontab): Centralize scheduling in a unique loop

--- src/Boilerwork/Attributes/Crontab/Crontab.php
+++ src/Boilerwork/Attributes/Crontab/Crontab.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 
 namespace Boilerwork\Attributes\Crontab;
 
+use App\Shared\Providers\MessagingProvider;
 use Attribute;
 use Boilerwork\Persistence\Adapters\Redis\RedisAdapter;
+use Boilerwork\Validation\Assert;
+use OpenSwoole\Coroutine;
 
 #[Attribute(Attribute::TARGET_CLASS)]
 final class Crontab
@@ -102,152 +105,12 @@ final class Crontab
      */
     const INTERVAL_SUNDAY = 'sunday';
 
-    private readonly RedisAdapter $redis;
-
-    public function __construct(string $interval, string $at = null, string $target = null)
+    public function __construct(
+        public readonly string  $interval,
+        public readonly ?string $at = null,
+        public readonly ?string $target = null
+    )
     {
-        go(function () use ($target, $interval, $at) {
-            $this->redis = globalContainer()->get(RedisAdapter::class);
-            while (true) {
-                $intervalSeconds = $this->getSecondsForInterval($interval, $at);
-
-                // Intenta obtener un bloqueo en Redis y establece un TTL en la clave
-                $key = sprintf('crontab:%s:%s:%s', env('APP_ENV'), env('APP_NAME'), $target);
-                $lockAcquired = $this->redis->set($key, 1, ['nx', 'ex' => $intervalSeconds]);
-
-                if ($lockAcquired) {
-                    try {
-                        $instance = globalContainer()->get($target);
-                        $instance->handle();
-
-                    } catch (\Throwable $e) {
-                        $errorMessage = "Error while executing task: " . $e->getMessage();
-                        error($errorMessage);
-                    }
-                }
-
-                // Get remaining TTL
-                $remainingTTL = $this->redis->ttl($key);
-
-                // If TTL is -1 (there is no TLL, use complete interval)
-                $sleepTime = $remainingTTL === -1 ? $intervalSeconds : $remainingTTL;
-
-                sleep($sleepTime);
-            }
-        });
     }
 
-    private function getSecondsForInterval(string $interval, string $at = null): int
-    {
-        return match ($interval) {
-            'everyMinute' => $this->getSecondsUntilNextMinute(),
-            'hourly' => $this->getSecondsUntilNextHour(),
-            'daily',
-            'dailyAt' => $this->getSecondsUntilNextDayAt(at: $at),
-            'weekly' => $this->getSecondsUntilNextMonday(),
-            'monday',
-            'tuesday',
-            'wednesday',
-            'thursday',
-            'friday',
-            'saturday',
-            'sunday' => $this->getSecondsUntilNextDayOfTheWeek(dayOfTheWeek: $interval),
-            default => throw new \InvalidArgumentException("Invalid interval: $interval"),
-        };
-    }
-
-    private function getSecondsUntilNextMinute(): int
-    {
-        $nextMinute = new \DateTime('1 minute');
-        $nextMinute->setTime((int)$nextMinute->format('H'), (int)$nextMinute->format('i'), 0);
-
-        $now = new \DateTime();
-
-        return $nextMinute->getTimestamp() - $now->getTimestamp();
-    }
-
-    private function getSecondsUntilNextHour(): int
-    {
-        $nextHour = new \DateTime('1 hour');
-        $nextHour->setTime((int)$nextHour->format('H'), 0, 0);
-        $now = new \DateTime();
-
-        return $nextHour->getTimestamp() - $now->getTimestamp();
-    }
-
-    /**
-     * Calculate the number of seconds until the next occurrence of a specific day of the week.
-     *
-     * This function calculates the number of seconds between the current time and the next
-     * occurrence of the specified day of the week. The day of the week should be provided
-     * as a string, such as 'monday', 'tuesday', etc.
-     *
-     * @param string $dayOfTheWeek The day of the week for which to calculate the next occurrence.
-     *                             This should be a string like 'monday', 'tuesday', etc.
-     * @return int The number of seconds until the next occurrence of the specified day of the week.
-     *
-     * @throws \Exception If there is an error parsing the day of the week string.
-     *
-     * @example
-     * // Get the number of seconds until next Monday.
-     * $seconds = $this->getSecondsUntilNextDayOfTheWeek('monday');
-     *
-     * // Get the number of seconds until next Friday.
-     * $seconds = $this->getSecondsUntilNextDayOfTheWeek('friday');
-     */
-    private function getSecondsUntilNextDayOfTheWeek(string $dayOfTheWeek): int
-    {
-        $nextDay = new \DateTime('next ' . $dayOfTheWeek);
-        $now = new \DateTime();
-
-        return $nextDay->getTimestamp() - $now->getTimestamp();
-    }
-
-    /**
-     * Get remaining secondes until next Monday at 00:00
-     */
-    private function getSecondsUntilNextMonday(): int
-    {
-        $nextMonday = new \DateTime('next monday');
-        $nextMonday->setTime(0, 0);
-        $now = new \DateTime();
-
-        return $nextMonday->getTimestamp() - $now->getTimestamp();
-    }
-
-    /**
-     * Calculate the number of seconds until the next day at a given time.
-     *
-     * This function calculates the number of seconds between the current time
-     * and the next day at a specific time. If no time is provided, it defaults
-     * to midnight. The time should be provided in the format 'HH:MM'.
-     *
-     * @param string|null $at The time at which the next day should be calculated,
-     *                        in the format 'HH:MM'. If null, defaults to '00:00'.
-     * @return int The number of seconds until the next day at the specified time.
-     *
-     * @throws \Exception If there is an error parsing the time string.
-     *
-     * @example
-     * // Get the number of seconds until the next day at midnight.
-     * $seconds = $this->getSecondsUntilNextDayAt();
-     *
-     * // Get the number of seconds until the next day at 13:30.
-     * $seconds = $this->getSecondsUntilNextDayAt('13:30');
-     */
-    private function getSecondsUntilNextDayAt($at = null): int
-    {
-        $nextMidnight = new \DateTime('tomorrow');
-
-        if ($at) {
-            list($hour, $minute) = explode(':', $at);
-            $nextMidnight->setTime((int)$hour, (int)$minute, 0);
-        } else {
-            $nextMidnight->setTime(0, 0, 0);
-        }
-
-        $now = new \DateTime();
-
-        return $nextMidnight->getTimestamp() - $now->getTimestamp();
-    }
 }
--- src/Boilerwork/Attributes/Crontab/CrontabScanner.php
+++ src/Boilerwork/Attributes/Crontab/CrontabScanner.php
@@ -5,18 +5,205 @@ declare(strict_types=1);
 
 namespace Boilerwork\Attributes\Crontab;
 
+use App\Shared\Providers\MessagingProvider;
+use Attribute;
 use Boilerwork\Attributes\AbstractScanner;
+use Boilerwork\Attributes\Routing\Route;
+use Boilerwork\Persistence\Adapters\Redis\RedisAdapter;
+use Boilerwork\Validation\Assert;
+use OpenSwoole\Coroutine;
+use OpenSwoole\Process;
 
+/**
+ * Scheduled Crons/Jobs are triggered immediately if they were never executed before and there is no block in Redis.
+ */
 final class CrontabScanner extends AbstractScanner
 {
     protected const ATTRIBUTE_CLASS = Crontab::class;
 
+    private array $tasks = [];
+
+    private readonly RedisAdapter $redis;
+
     protected function processAttribute(\ReflectionAttribute $attribute, $parentClass = null): void
     {
-        new Crontab(
-            interval: $attribute->getArguments()['interval'],
-            at: $attribute->getArguments()['at'] ?? null,
-            target: $parentClass->getName(),
-        );
+        $this->tasks[] = [
+            'arguments' => $attribute->getArguments(),
+            'target' => $parentClass->getName(),
+        ];
+    }
+
+    /**
+     * Overwrite parent method to add this in a separate Process
+     */
+    public function scan(string $directory): void
+    {
+        $process = new \OpenSwoole\Process(function ($worker) use ($directory) {
+            parent::scan($directory);
+            go(function () {
+                $this->run();
+            });
+        });
+
+        $process->start();
+    }
+
+    private function run(): void
+    {
+        if(count($this->tasks) === 0)
+        {
+            return;
+        }
+
+        // No podemos inyectarlo en el constructor al estar en un proceso independiente.
+        $this->redis = globalContainer()->get(RedisAdapter::class);
+
+        while (true) {
+            foreach ($this->tasks as $task) {
+
+                $crontab = new Crontab(
+                    interval: $task['arguments']['interval'],
+                    at: $task['arguments']['at'] ?? null,
+                    target: $task['target'],
+                );
+
+                $key = sprintf('crontab:%s:%s:%s', env('APP_ENV'), env('APP_NAME'), $crontab->target);
+
+                $intervalSeconds = $this->getSecondsForInterval(interval: $crontab->interval, at: $crontab->at);
+
+                // Obtiene un bloqueo en Redis y establece un TTL en la clave si no existe previamente.
+                $lockAcquired = $this->redis->set($key, 1, ['nx', 'ex' => $intervalSeconds]);
+
+                if ($lockAcquired) {
+                    go(function () use ($crontab) {
+                        try {
+                            $instance = globalContainer()->get($crontab->target);
+                            $instance->handle();
+                        } catch (\Throwable $e) {
+                            $errorMessage = "Error while executing task: " . $e->getMessage();
+                            error($errorMessage);
+                        }
+                    });
+                }
+            }
+
+            sleep(1);
+        }
+    }
+
+    private function getSecondsForInterval(string $interval, string $at = null): int
+    {
+        return match ($interval) {
+            Crontab::INTERVAL_EVERY_MINUTE => $this->getSecondsUntilNextMinute(),
+            Crontab::INTERVAL_HOURLY => $this->getSecondsUntilNextHour(),
+            Crontab::INTERVAL_DAILY,
+            Crontab::INTERVAL_DAILY_AT => $this->getSecondsUntilNextDayAt(at: $at),
+            Crontab::INTERVAL_WEEKLY => $this->getSecondsUntilNextMonday(),
+            Crontab::INTERVAL_MONDAY,
+            Crontab::INTERVAL_TUESDAY,
+            Crontab::INTERVAL_WEDNESDAY,
+            Crontab::INTERVAL_THURSDAY,
+            Crontab::INTERVAL_FRIDAY,
+            Crontab::INTERVAL_SATURDAY,
+            Crontab::INTERVAL_SUNDAY => $this->getSecondsUntilNextDayOfTheWeek(dayOfTheWeek: $interval),
+            default => throw new InvalidArgumentException("Invalid interval: $interval"),
+        };
+    }
+
+    private function getSecondsUntilNextMinute(): int
+    {
+        $nextMinute = new \DateTime('1 minute');
+        $nextMinute->setTime((int)$nextMinute->format('H'), (int)$nextMinute->format('i'), 0);
+
+        $now = new \DateTime();
+
+        return $nextMinute->getTimestamp() - $now->getTimestamp();
+    }
+
+    private function getSecondsUntilNextHour(): int
+    {
+        $nextHour = new \DateTime('1 hour');
+        $nextHour->setTime((int)$nextHour->format('H'), 0, 0);
+        $now = new \DateTime();
+
+        return $nextHour->getTimestamp() - $now->getTimestamp();
+    }
+
+    /**
+     * Calculate the number of seconds until the next occurrence of a specific day of the week.
+     *
+     * This function calculates the number of seconds between the current time and the next
+     * occurrence of the specified day of the week. The day of the week should be provided
+     * as a string, such as 'monday', 'tuesday', etc.
+     *
+     * @param string $dayOfTheWeek The day of the week for which to calculate the next occurrence.
+     *                             This should be a string like 'monday', 'tuesday', etc.
+     * @return int The number of seconds until the next occurrence of the specified day of the week.
+     *
+     * @throws \Exception If there is an error parsing the day of the week string.
+     *
+     * @example
+     * // Get the number of seconds until next Monday.
+     * $seconds = $this->getSecondsUntilNextDayOfTheWeek('monday');
+     *
+     * // Get the number of seconds until next Friday.
+     * $seconds = $this->getSecondsUntilNextDayOfTheWeek('friday');
+     */
+    private function getSecondsUntilNextDayOfTheWeek(string $dayOfTheWeek): int
+    {
+        $nextDay = new \DateTime('next ' . $dayOfTheWeek);
+        $now = new \DateTime();
+
+        return $nextDay->getTimestamp() - $now->getTimestamp();
+    }
+
+    /**
+     * Get remaining secondes until next Monday at 00:00
+     */
+    private function getSecondsUntilNextMonday(): int
+    {
+        $nextMonday = new \DateTime('next monday');
+        $nextMonday->setTime(0, 0);
+        $now = new \DateTime();
+
+        return $nextMonday->getTimestamp() - $now->getTimestamp();
+    }
+
+    /**
+     * Calculate the number of seconds until the next day at a given time.
+     *
+     * This function calculates the number of seconds between the current time
+     * and the next day at a specific time. If no time is provided, it defaults
+     * to midnight. The time should be provided in the format 'HH:MM'.
+     *
+     * @param string|null $at The time at which the next day should be calculated,
+     *                        in the format 'HH:MM'. If null, defaults to '00:00'.
+     * @return int The number of seconds until the next day at the specified time.
+     *
+     * @throws \Exception If there is an error parsing the time string.
+     *
+     * @example
+     * // Get the number of seconds until the next day at midnight.
+     * $seconds = $this->getSecondsUntilNextDayAt();
+     *
+     * // Get the number of seconds until the next day at 13:30.
+     * $seconds = $this->getSecondsUntilNextDayAt('13:30');
+     */
+    private function getSecondsUntilNextDayAt($at = null): int
+    {
+        $nextMidnight = new \DateTime('tomorrow');
+
+        if ($at) {
+            list($hour, $minute) = explode(':', $at);
+            $nextMidnight->setTime((int)$hour, (int)$minute, 0);
+        } else {
+            $nextMidnight->setTime(0, 0, 0);
+        }
+
+        $now = new \DateTime();
+
+        var_dump($now->format('H:i:s'));
+
+        return $nextMidnight->getTimestamp() - $now->getTimestamp();
     }
 }
